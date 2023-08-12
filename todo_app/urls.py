2b95bae9919360852191d7d15683a16a954e09a0
Md Hridoy Hossain <hridoythebest@gmail.com>
2023-08-12 11:31:08 +0600

    Added Templates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views
+from .views import ShowTasksView, CompletedTasksView, AddTaskView, EditTaskView, DeleteTaskView, complete_task
 
 
-urlspatterns = [
-    path('', views.home),
-    
+
+
+urlpatterns = [
+    path('show_tasks', ShowTasksView.as_view(), name='show_tasks'),
+    path('completed_tasks', CompletedTasksView.as_view(), name='completed_tasks'),
+    path('add_task', AddTaskView.as_view(), name='add_task'),
+    path('edit_task/<int:pk>', EditTaskView.as_view(), name='edit_task'),
+    path('delete_task/<int:pk>', DeleteTaskView.as_view(), name='delete_task'),
+    path('complete_task/<int:pk>', complete_task, name='complete_task'),
 ]
